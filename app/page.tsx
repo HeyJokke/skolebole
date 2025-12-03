88c8c42af75e887aa8f5598d78ab86e11f41d74e
HeyJokke <Jokke.k992@gmail.com>
2025-12-03 21:17:49 +0100

    Navigation buttons on Home component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Link from 'next/link'
 
 export default async function Home() {
 
+  const linkClass = "bg-[url(/images/dansk_flag.jpg)] bg-cover overflow-hidden h-50 w-50 m-auto flex justify-center items-center border-2 border-red-500 rounded-full text-center font-bold hover:border-red-400 hover:bg-gray-100 transition duration-250 hover:scale-102"
+  const pFlagClass = "backdrop-blur-xs transition duration-100 hover:backdrop-blur-none flex w-full h-full justify-center items-center text-3xl"
+
   return (
     <main className="h-full min-h-screen flex m-5">
       <div className="w-full">
@@ -15,11 +18,25 @@ export default async function Home() {
               Sed sagittis vitae velit pellentesque commodo. Cras lacus leo, commodo nec ex nec, auctor mattis enim. Donec at sem at ante finibus convallis quis a tellus. Phasellus commodo posuere magna, sit amet commodo ipsum sodales quis. Etiam et nisi neque. Pellentesque vitae nulla a odio congue egestas. Proin eu felis vitae sem pellentesque dignissim vitae vel lacus. Donec ut eros at metus hendrerit posuere. Etiam ut nisi id urna vestibulum fermentum. Aenean metus sem, aliquam eu turpis id, hendrerit aliquet ex. Vestibulum blandit erat justo, eget tristique libero condimentum et. Pellentesque viverra eros elit, nec tempor nisl euismod eget. Morbi odio felis, venenatis a quam in, dapibus imperdiet mi.
             </p>
             <div className="m-auto grid grid-cols-2 gap-6 lg:w-130 sm:w-100">
-              <Link className="bg-gray-200 h-50 w-50 m-auto flex justify-center items-center border-2 border-red-500 rounded-full text-center text-red-700 font-bold hover:border-red-400 hover:text-red-500 hover:bg-gray-100 transition duration-250 hover:scale-102" href="materialer?kategori=dansk">Dansk</Link>
-              <Link className="bg-gray-200 h-50 w-50 m-auto flex justify-center items-center border-2 border-red-500 rounded-full p-5 text-center text-red-700 font-bold hover:border-red-400 hover:text-red-500 hover:bg-gray-100 transition duration-250 hover:scale-102" href="materialer?kategori=matematik">Matematik</Link>
-              <Link className="bg-gray-200 h-50 w-50 m-auto flex justify-center items-center border-2 border-red-500 rounded-full p-5 text-center text-red-700 font-bold hover:border-red-400 hover:text-red-500 hover:bg-gray-100 transition duration-250 hover:scale-102" href="materialer?kategori=engelsk">Engelsk</Link>
-              <Link className="bg-gray-200 h-50 w-50 m-auto flex justify-center items-center border-2 border-red-500 rounded-full p-5 text-center text-red-700 font-bold hover:border-red-400 hover:text-red-500 hover:bg-gray-100 transition duration-250 hover:scale-102" href="materialer?kategori=naturteknik">Natur & Teknik</Link>
-              <Link className="bg-gray-200 h-50 w-50 m-auto flex justify-center items-center border-2 border-red-500 rounded-full p-5 text-center text-red-700 font-bold hover:border-red-400 hover:text-red-500 hover:bg-gray-100 transition duration-250 hover:scale-102" href="materialer?kategori=historie">Historie</Link>
+              <Link className={`bg-[url(/images/dansk_flag.jpg)] ${linkClass}`} href="materialer?kategori=dansk">
+                <p className={pFlagClass}>Dansk</p>
+              </Link>
+              
+              <Link className={`bg-[url(/images/matematik_flag.png)] ${linkClass}`} href="materialer?kategori=matematik">
+                <p className={pFlagClass}>Matematik</p>
+              </Link>
+
+              <Link className={`bg-[url(/images/engelsk_flag.png)] ${linkClass}`} href="materialer?kategori=engelsk">
+                <p className={pFlagClass}>Engelsk</p>
+              </Link>
+
+              <Link className={`bg-[url(/images/naturteknik_flag.png)] ${linkClass}`} href="materialer?kategori=naturteknik">
+                <p className={pFlagClass}>Natur & Teknik</p>
+              </Link>
+
+              <Link className={`bg-[url(/images/historie_flag.png)] ${linkClass}`} href="materialer?kategori=historie">
+                <p className={pFlagClass}>Historie</p>
+              </Link>
             </div>
           </div>
           <HighlightMaterials title={"Tilføjet for nyligt"} vertical={true} amount={4}/>
