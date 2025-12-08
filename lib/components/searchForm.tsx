e067dc193ea5530d1465e97fe0f048f24909e17e
HeyJokke <Jokke.k992@gmail.com>
2025-12-08 09:25:06 +0100

    Update searchForm.tsx
    Removed router.push when input field was empty

--- lib/components/searchForm.tsx
+++ lib/components/searchForm.tsx
@@ -31,17 +31,19 @@ export default function SearchForm() {
             nextParams.delete('kategori')
             nextParams.set('q', searchTerm)
             router.push(`/materialer/search?${nextParams.toString()}`)
-        } else {
-            router.push('/materialer')
-        }
-
-        
+        }    
     }
 
     return (
         <form className="flex justify-center lg:mt-0 mt-5" onSubmit={onSubmit}>
             <div className="flex items-center pl-2 pt-1 pb-1 bg-gray-100 text-black rounded-lg">
-                <svg className="w-6 h-7 text-body text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/></svg>
+                <button className="cursor-pointer" type="submit">
+                    <svg 
+                        className="w-6 h-7 text-body text-gray-500" 
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </button>
                 <input
                     className="ml-2 outline-none"  
                     placeholder="Søg..."
