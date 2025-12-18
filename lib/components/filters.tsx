912ef35ec146fa5cfcbabd2b42bfc768e0f9955a
HeyJokke <Jokke.k992@gmail.com>
2025-12-18 15:30:41 +0100

    Styling

--- lib/components/filters.tsx
+++ lib/components/filters.tsx
@@ -43,11 +43,11 @@ export default function Filters({filteredMaterials}: FilterProps):React.ReactEle
         <main className='mb-10 md:mr-10 min-w-[200]'>
             <h2 className="text-xl font-bold">Sortér efter:</h2>
             {(!loading && !error) && <ul className="min-w-[150px]">
-                <li onClick={() => handleClick(null)} className={`cursor-pointer ml-1 mt-1 p-2 text-slate-700 font-extrabold text-lg hover:scale-105 transform-size duration-200 bg-white/70 hover:bg-white rounded-xl w-fit`}>
+                <li onClick={() => handleClick(null)} className={`cursor-pointer ml-1 mt-1 p-2 text-slate-700 font-extrabold hover:scale-105 transform-size duration-200 bg-white/70 hover:bg-white rounded-xl w-full md:w-fit text-xl md:text-base text-center`}>
                     Nulstil
                 </li>
                 {uniqueCategories.sort((a,b) => a.localeCompare(b)).map(cat => (
-                        <li onClick={() => handleClick(cat)} className={`cursor-pointer ml-1 mt-1 p-2 text-slate-700 font-extrabold text-lg hover:scale-105 transform-size duration-200 rounded-xl w-fit ${params.get('kategori') === cat ? 'bg-blue-200 shadow-md' : "bg-white/70 hover:bg-white"}`} key={cat}>
+                        <li onClick={() => handleClick(cat)} className={`cursor-pointer ml-1 mt-1 p-2 text-slate-700 font-extrabold hover:scale-105 transform-size duration-200 rounded-xl w-full md:w-fit text-xl md:text-base text-center ${params.get('kategori') === cat ? 'bg-blue-200 shadow-md' : "bg-white/70 hover:bg-white"}`} key={cat}>
                             {cat[0].toUpperCase() + cat.slice(1)}
                         </li>
                 ))}
