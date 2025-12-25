a9cab390dff543e026e1bcd7877d21d18fbac4da
HeyJokke <Jokke.k992@gmail.com>
2025-12-25 14:33:15 +0100

    HomePage larger text

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,14 @@ export default async function Home() {
         <div className="lg:flex md:block text-slate-700">
           <div className="bg-white/90 rounded-lg shadow-xl mr-0 lg:mr-10 p-8">
             <h1 className="text-5xl font-bold text-center mb-5">Velkommen til <br/> SkoleBole</h1>
-            <p className="lg:mr-5 mb-10 text-base/7">
+            <p className="lg:mr-5 mb-10 text-lg/7">
               Tak fordi du kigger forbi hos Forlaget Kluddermors nye lillebror SkoleBole, som er en enkel og overskuelig materialeplatform med printklare kopisider til de yngste elever. Her finder du små opgaver, der kan bruges enkeltvis og tages i brug med det samme – særligt på de dage, hvor dagens program ændrer sig, der er færre voksne, eller en vikar har brug for en klar og enkel opgave her og nu.
+              <br/>
               Materialerne hos SkoleBole er skabt som et supplement til undervisningen. Opgaverne står alene, kræver ingen forberedelse og egner sig godt som morgenopgaver, ekstraopgaver og til repetition, hvor eleverne genøver kendte færdigheder i deres eget tempo.
               SkoleBole er samtidig et uforpligtende og legende udviklingsrum. Her eksperimenteres med opgavetyper, udtryk og formater, og der er plads til oldschool kopisider, hvor ikke alt behøver indgå i et større system. Nogle af materialerne er ligeledes forenklede udpluk inspireret af Kluddermors mest anvendte opgavetyper.
             </p>
             <h2 className="text-2xl mb-2 font-bold">Det får du hos SkoleBole:</h2>
-              <ul className="list-disc list-inside text-base/7">
+              <ul className="list-disc list-inside text-lg/7">
                 <li>Små, selvstændige kopisider i PDF-format</li>
                 <li>Opgaver, der kan bruges her og nu</li>
                 <li>Velegnet til morgenstart, ekstra tid, vikar og repetition</li>
@@ -22,19 +23,19 @@ export default async function Home() {
               </ul>
             <br/>
             <h2 className="text-2xl mb-2 font-bold">Det får du <span className="text-italic">ikke</span> hos SkoleBole:</h2>
-              <ul className="list-disc list-inside text-base/7">
+              <ul className="list-disc list-inside text-lg/7">
                 <li>Et samlet læseprogram</li>
                 <li>Materialer med fast progression og trin-for-trin-opbygning</li>
                 <li>Forløb med differentiering og lærervejledninger</li>
                 <li>Lamineringsmaterialer, større kopimapper eller spil</li>
               </ul>
             <br/>
-            <p className="lg:mr-5 mb-10 text-base/7">
+            <p className="lg:mr-5 mb-10 text-lg/7">
               Har dine elever brug for et gennemarbejdet læseprogram med tydelig progression, struktur og differentiering, finder du det stadig hos Forlaget Kluddermor, der samler det hele i ét sammenhængende system. SkoleBole er stedet for de små opgaver, der skal ligge klar, når noget opstår – og til de idéer, der gerne må prøves af uden store krav eller bindinger.
               <br/><br/>
               De bedste hilsner<br/>
               Liselotte Ring Kryger<br/>
-              Forlægger & Forfatter på<br/>
+              Forlægger & forfatter på<br/>
               SkoleBole og Forlaget Kluddermor
             </p>
           </div>
