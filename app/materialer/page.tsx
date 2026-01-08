f8615ffef3cac079097fe2fbbe7f347e7f45a8b5
HeyJokke <Jokke.k992@gmail.com>
2026-01-08 19:16:38 +0100

    Changed materialer page

--- app/materialer/page.tsx
+++ app/materialer/page.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 
 export default function Materialer():React.ReactElement {
-    const linkClass = "bg-[url(/images/dansk_flag.jpg)] bg-cover overflow-hidden h-50 w-50 m-auto flex justify-center items-center border-2 border-red-500 rounded-full text-center font-bold hover:border-red-400 hover:bg-gray-100 transition duration-250 hover:scale-102"
+    const linkClass = "bg-cover overflow-hidden h-[250px] w-[250px] m-auto flex justify-center items-center border-2 border-red-500 rounded-full text-center font-bold hover:border-red-400 hover:bg-gray-100 transition duration-250 hover:scale-102"
     //const pFlagClass = "backdrop-blur-xs transition duration-100 hover:backdrop-blur-none flex w-full h-full justify-center items-center text-xl"
     
     return (
@@ -14,17 +14,17 @@ export default function Materialer():React.ReactElement {
               
               <Link className={`bg-[url(/images/matematik_boble.png)] ${linkClass}`} href="materialer/matematik" />
 
-              <Link className={`bg-[url(/images/morgenopgaver_boble.png)] ${linkClass}`} href="materialer/morgenopgaver" />
-
               <Link className={`bg-[url(/images/bhkl_boble.png)] ${linkClass}`} href="materialer/børnehaveklasse" />
+
+              <Link className={`bg-[url(/images/1klasse_boble.png)] ${linkClass}`} href="materialer/1.klasse" />
+              
+              <Link className={`bg-[url(/images/DSA_boble.png)] ${linkClass}`} href="materialer/dsa" />
 
               <Link className={`bg-[url(/images/specunderv_boble.png)] ${linkClass}`} href="materialer/specialundervisning" />
 
+              <Link className={`bg-[url(/images/morgenopgaver_boble.png)] ${linkClass}`} href="materialer/morgenopgaver" />
+
               <Link className={`bg-[url(/images/demo_boble.png)] ${linkClass}`} href="materialer/demoopgaver" />
-
-              <Link className={`bg-[url(/images/DSA_boble.png)] ${linkClass}`} href="materialer/dsa" />
-
-              <Link className={`bg-[url(/images/1klasse_boble.png)] ${linkClass}`} href="materialer/1.klasse" />
             </div>
         </main>
     )
