4610bc0389542084adb9d7a93ae08d2d31624a4c
HeyJokke <Jokke.k992@gmail.com>
2026-01-12 18:14:36 +0100

    Styling

--- app/materialer/[section]/[id]/page.tsx
+++ app/materialer/[section]/[id]/page.tsx
@@ -71,7 +71,7 @@ export default function ProductPage({ params }:
                         </div>
                     </header>
                     <div className="lg:flex sm:block mt-4 p-4 md:p-8">
-                        <div className="block lg:w-2/5 sm:w-full m-auto">
+                        <div className="block lg:w-2/5 sm:w-full mr-auto ml-auto">
                             <Image 
                                 alt={`Produktbillede for ${m.name}`} 
                                 src={m.image_path ? m.image_path : '/images/skolebole_fallback.png'}
@@ -82,7 +82,7 @@ export default function ProductPage({ params }:
                             
                         </div>
                         <div className="lg:w-3/5 sm:w-full">
-                            <pre className="lg:ml-10 text-base/6 text-clip mt-3 text-wrap font-sans">{m.long_description}</pre>
+                            <pre className="lg:ml-10 text-base/6 text-clip mt-5 lg:mt-0 text-wrap font-sans">{m.long_description}</pre>
                             <button onClick={() => pdfRedirect(m)} className="flex items-center justify-center lg:ml-10 mt-10 w-40 rounded-lg h-15 bg-green-300 text-green-900 font-bold hover:cursor-pointer hover:text-white hover:bg-green-500">
                                 <IoMdDownload className="mr-1 text-xl"/> 
                                 Download
--- lib/components/materialCard.tsx
+++ lib/components/materialCard.tsx
@@ -26,7 +26,7 @@ export default function MaterialCard({m}: CardProps) {
     return (
         <Link href={`${pathName}/${m.id}`}>
             <div className="bg-white rounded-xl shadow-md hover:shadow-xl transition-shadow duration-300 overflow-hidden hover:scale-105 transform transition-transform">
-                <div className="relative w-full h-36 bg-gray-200">
+                <div className="relative w-full h-30 bg-gray-200">
                     <Image 
                         alt={`Produkt billede til ${m.name}`} 
                         src={m.image_path ? m.image_path : '/images/skolebole_fallback.png'} 
@@ -35,12 +35,12 @@ export default function MaterialCard({m}: CardProps) {
                         className="w-full h-full object-cover"
                     />
                 </div>
-                <div className="p-4 flex flex-col">
+                <div className="p-4 flex flex-col min-h-[236]">
                     <h3 className="text-lg font-semibold text-gray-800 mb-2 text-pretty">{m.name}</h3>
-                    <div className="overflow-y-auto h-full justify-between">
-                        <p className="text-sm text-gray-600 mb-3 min-h-[40] line-clamp-2">{m.short_description[0].toUpperCase() + m.short_description.slice(1)}</p>
+                    <div className="flex flex-col flex-grow">
+                        <p className="text-sm text-gray-600 mb-3 line-clamp-2">{m.short_description[0].toUpperCase() + m.short_description.slice(1)}</p>
                         {m.categories_array && (
-                            <div className="flex flex-wrap gap-2 min-h-[56]">
+                            <div className="flex flex-wrap gap-2 min-h-[56] mt-auto">
                                 {m.categories_array.sort((a:string, b:string) => a.localeCompare(b)).map((cat:string) => (
                                     <span 
                                         key={cat} 
