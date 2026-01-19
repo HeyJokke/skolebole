8e4ec4491e22daaa2c76d5c3c231769bd3452b18
HeyJokke <Jokke.k992@gmail.com>
2026-01-19 16:15:11 +0100

    Update page.tsx
    Responsive design on button

--- app/materialer/[section]/[id]/page.tsx
+++ app/materialer/[section]/[id]/page.tsx
@@ -70,7 +70,7 @@ export default function ProductPage({ params }:
                             </div>
                         </div>
                     </header>
-                    <div className="lg:flex sm:block mt-4 p-4 md:p-8">
+                    <div className="lg:flex block mt-4 p-4 md:p-8">
                         <div className="block lg:w-2/5 sm:w-full mr-auto ml-auto">
                             <Image 
                                 alt={`Produktbillede for ${m.name}`} 
@@ -81,9 +81,9 @@ export default function ProductPage({ params }:
                             />
                             
                         </div>
-                        <div className="lg:w-3/5 sm:w-full">
+                        <div className="lg:w-3/5 w-full">
                             <pre className="lg:ml-10 text-base/6 text-clip mt-5 lg:mt-0 text-wrap w-full wrap-break-word font-sans">{m.long_description}</pre>
-                            <button onClick={() => pdfRedirect(m)} className="flex items-center justify-center lg:ml-10 mt-10 w-40 rounded-lg h-15 bg-green-300 text-green-900 font-bold hover:cursor-pointer hover:text-white hover:bg-green-500">
+                            <button onClick={() => pdfRedirect(m)} className="flex items-center justify-center lg:ml-10 mt-10 w-full lg:w-40 rounded-lg h-15 bg-green-300 text-green-900 font-bold hover:cursor-pointer hover:text-white hover:bg-green-500">
                                 <IoMdDownload className="mr-1 text-xl"/> 
                                 Download
                             </button>
