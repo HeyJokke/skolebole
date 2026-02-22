affee7c908f6608d42abfaf9f17ec4d19db16ddd
HeyJokke <Jokke.k992@gmail.com>
2026-02-22 20:44:10 +0100

    Updated renderMaterials
    Changed grid cols to 2 on phone

--- lib/components/renderMaterials.tsx
+++ lib/components/renderMaterials.tsx
@@ -25,7 +25,7 @@ export default function RenderMaterials({materials, error, loading}: Props) {
 
     return (
         <main className="md:flex">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-6">
                 {html}
             </div>
         </main>
