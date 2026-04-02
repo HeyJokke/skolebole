11f239c7ab33533040617ef309be9bf100b80d48
HeyJokke <Jokke.k992@gmail.com>
2026-04-02 12:54:03 +0200

    Update PreviousPage.tsx

--- lib/components/PreviousPage.tsx
+++ lib/components/PreviousPage.tsx
@@ -1,21 +1,21 @@
 "use client"
 
+import { useRouter, usePathname } from 'next/navigation'
 import { FaArrowLeft } from "react-icons/fa";
 
+
 export default function PreviousPage() {
-    function handleBack() {
-        if (window.history.length > 1) {
-            window.history.back();
-        } else {
-            window.location.href = '/';
-        }
-    }
+    const router = useRouter()
+    const pathName = usePathname()
+    const pathArr = pathName.split('/')
+    pathArr.pop()
+    const lastPath = pathArr.join('/')
 
     return (
-        <button
-            onClick={handleBack}
+        <button 
+            onClick={() => lastPath.includes('search') ? router.back() : router.push(lastPath && lastPath != '/materialer' ? lastPath : "/")} 
             className="flex mt-3 items-center mb-5 hover:underline hover:cursor-pointer hover:scale-105">
-            <FaArrowLeft className="mr-2"/>
+            <FaArrowLeft className="mr-2"/> 
             Tilbage
         </button>
     )
