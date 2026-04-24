5e355c451ce96dd43a715b7c30b51ca299008987
HeyJokke <Jokke.k992@gmail.com>
2026-04-24 17:03:34 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -10,7 +10,8 @@ export default function robots(): MetadataRoute.Robots {
             },
             {
                 userAgent: 'facebookexternalhit',
-                allow: ['/admin', '/materialer/search/'],
+                allow: '/',
+                disallow: ['/admin', '/materialer/search/'],
             }
         ],
         sitemap: 'https://skolebole.dk/sitemap.xml',
